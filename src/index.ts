af369d62644068206765ce947c8c5107693131fc
elee <eddy@gfxlabs.io>
2021-09-05 02:51:36 -0500

    updated

--- src/index.ts
+++ src/index.ts
@@ -13,17 +13,25 @@ const provider = new ethers.providers.JsonRpcProvider("https://polygon-rpc.com",
 const district = new ethers.Contract("0x83537906b8501C2843bDe7636E7f0dF0d1daB5eD",abis.district,provider);
 const fromBlock = 18761008;
 
-const updateFrom = async (startBlock:number):Promise<any[]>=>{
+const grabPlot = async (startBlock:number):Promise<any[]>=>{
   return district.queryFilter(district.filters.PlotTransfer(),startBlock)
 }
 
-router.get("/:block", async (ctx,next)=>{
-  console.log(ctx.params.block)
+const grabDistrict = async (startBlock:number):Promise<any[]>=>{
+  return district.queryFilter(district.filters.Transfer(),startBlock)
+}
+
+router.get("/plot/:block", async (ctx,next)=>{
   const lesser = parseInt(ctx.params.block) > fromBlock ? parseInt(ctx.params.block) : fromBlock;
   ctx.status = 200;
-  ctx.body = await updateFrom(lesser)
+  ctx.body = await grabPlot(lesser)
 })
 
+router.get("/district/:block", async (ctx,next)=>{
+  const lesser = parseInt(ctx.params.block) > fromBlock ? parseInt(ctx.params.block) : fromBlock;
+  ctx.status = 200;
+  ctx.body = await grabDistrict(lesser)
+})
 
 app.use(router.routes()).use(router.allowedMethods()).listen(10100);
 console.log("running on port 10100");
